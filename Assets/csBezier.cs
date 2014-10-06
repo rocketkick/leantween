fa536b598084d33f40502b482f95a1d88c4b20b1
rocketkick <grad@nate.com>
2014-10-06 16:22:36 +0900

    Signed-off-by: rocketkick <grad@nate.com>

---
+++ Assets/csBezier.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csBezier : MonoBehaviour {
+
+	LTBezierPath ltPath=new LTBezierPath(new Vector3[]{new Vector3(0f,0f,0f),new Vector3(3f,0f,0f),new Vector3(3f,0f,0f),new Vector3(3f,0f,3f)});
+
+	// Use this for initialization
+	void Start () {
+
+		LeanTween.move(gameObject,ltPath.pts,4f).setOrientToPath(false).setDelay(1f).setEase(LeanTweenType.easeInOutQuad);
+		//Vector3 pt = ltPath.point(0.6f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+LeanTween
+=========
+
+LeanTween is an efficient tweening engine for the Unity 3d engine
+
+**To use LeanTween** move LeanTween.cs (inside the Assets/Plugins folder) into your Plugins directory of your current Unity project.
+
+Documentation can be found here: http://dentedpixel.com/LeanTweenDocumentation/classes/LeanTween.html
+
+Examples here: https://github.com/dentedpixel/LeanTween/archive/master.zip
